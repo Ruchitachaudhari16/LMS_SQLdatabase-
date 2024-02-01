62c1feda9136dd80ea2b847deff6f1911d048228
Ruchita Chaudhari <123711885+Ruchitachaudhari16@users.noreply.github.com>
2024-02-01 11:09:51 +0530

    Update queries.sql

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -4,4 +4,4 @@ SELECT fellowship_candidate.first_name,fellowship_candidate.last_name
 FROM fellowship_candidate
 JOIN tech_stack 
 ON fellowship_candidate.cic_id=tech_stack.Id
-where tech_stack.tech_name='j'
+where tech_stack.tech_name='Java'
